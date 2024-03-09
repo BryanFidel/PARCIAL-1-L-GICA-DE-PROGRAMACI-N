96e1ea0e91b55498bed6ad911680a67835a4f552
Bryan Fidel Huaynalaya Rios <brayanfid@gmail.com>
2024-03-08 21:19:32 -0500

    Tiempo en que el primer tren ha estado viajando antes de que salga el segundo tren

---
+++ punto2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace punto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan tiempoPrimero = TimeSpan.FromHours(1);
+        }
+    }
+}
