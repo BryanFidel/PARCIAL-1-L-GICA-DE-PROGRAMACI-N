bb3d00ca7549c3593ded07a4e8f9845bdf743c13
Bryan Fidel Huaynalaya Rios <brayanfid@gmail.com>
2024-03-08 20:30:59 -0500

    guardado de datos

--- punto1/Program.cs
+++ punto1/Program.cs
@@ -10,6 +10,10 @@ namespace punto1
             const double porcentajeComision = 0.10;
             const double objetivoVentas = 1000000;
             const double beneficioExtra = 100000;
+
+            double[] ventas = new double[3];
+            double totalComisiones = 0;
+            double totalVentas = 0;
         }
     }
 }
